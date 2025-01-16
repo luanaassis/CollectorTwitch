a62eec8f7393a73d5a4ca399c3ba33cc137acbd8
flaviosoriano <flaviosoriano0@gmail.com>
2025-01-16 18:06:13 -0300

    quase pronto pro treino

--- Persona/persona.py
+++ Persona/persona.py
@@ -9,22 +9,50 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-
-
 import time
 
-#Conta 1 - recem criada, sem preparação prévia, assite 1 live antes de utilizar o selenium
-nascimento = "10/06/2010"
+
+jogosLivre = {"Minecraft"}
+jogos10 = {"ROBLOX"}
+jogos12 = {"Fortnite", "Sea of Thieves", "The Sims 4", "League of Legends", "Overwatch 2"}
+jogos14 = {"Valorant"}
+
+# 0 = 0 - 10
+# 1 = 10 - 13
+# 2 = 13 - 17
+# 3 = 18+
+faixaEtaria = 2
+
+# Area das variáveis específicas de cada faixa etária
+if faixaEtaria == 0:
+    #Talvez tenha que mudar a varivel para que em toda ocorrencia randomize o numero
+    tempoDeVisualizacao = random.uniform(450, 900) # Em segundos
+    jogosAssistir = jogosLivre
+
+if faixaEtaria == 1:
+    tempoDeVisualizacao = random.uniform(600, 600) # Em segundos
+    jogosAssistir = jogosLivre.union(jogos10)
+
+if faixaEtaria == 2:
+    tempoDeVisualizacao = random.uniform(900,   3600) # Em segundos
+    jogosAssistir = jogosLivre.union(jogos10, jogos12, jogos14)
+
+if faixaEtaria == 3:
+    tempoDeVisualizacao = 3600 # Em segundos
+    jogosAssistir = "A DEFINIR"
+
+# Area das variáveis específicas de cada persona
+
+diretorio_perfil = "--profile-directory=Profile 6"
 google_login = "crianca1LOCUS@gmail.com"
 google_password = "Superben10!"
 twitch_username = "crianca0101"
 twitch_password = "Superben10!"
 
-
 # Configurar o WebDriver
 chromeOptions = Options()
+chromeOptions.add_argument(diretorio_perfil)
 chromeOptions.add_argument("user-data-dir=/home/flavio/.config/google-chrome")
-chromeOptions.add_argument("--profile-directory=Profile 6")
 chromeOptions.add_argument("--window-size=1280,800")
 chromeOptions.add_argument("--disable-extensions")
 chromeOptions.add_argument("--disable-notifications")
@@ -106,6 +134,47 @@ def LoginTwitch(driver):
         pass
 
 
+def Treino():
+
+    jogoPesquisado = random.choice(list(jogosAssistir))
+
+   
+    barraBusca = driver.find_element('css selector', '[placeholder="Buscar"]')
+    barraBusca.send_keys(jogoPesquisado)
+    
+    time.sleep(random.uniform(1.0, 2.0))
+
+    barraBusca.send_keys(Keys.RETURN)
+
+    time.sleep(random.uniform(1.5, 2.5))
+
+    """ 
+    CSS SELECTOR DOS 3 PRIMEIROS RESULTADOS DE PESQUISA (FIREFOX)
+    .search-results > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > strong:nth-child(1) > a:nth-child(1)
+    .search-results > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > strong:nth-child(1) > a:nth-child(1)
+    .search-results > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > strong:nth-child(1) > a:nth-child(1)
+    """
+
+    """
+    XPATH DOS 3 PRIMEIROS RESULTADOS DE PESQUISA (CHROME)
+    //*[@id="root"]/div/div[1]/div/main/div[1]/div[3]/div/div/div/div/div[1]/div[1]/div[2]/div[1]/div/div/div/div[1]/div/strong/a
+    //*[@id="root"]/div/div[1]/div/main/div[1]/div[3]/div/div/div/div/div[1]/div[1]/div[3]/div/div/div/div/div[1]/div/strong/a
+    //*[@id="root"]/div/div[1]/div/main/div[1]/div[3]/div/div/div/div/div[1]/div[1]/div[4]/div/div/div/div/div[1]/div/strong/a
+    """
+    #videoAssistido = random.randint(1, 3)
+
+    #CONSERTAR, PAROU DE FUNCIONAR
+    #Talvez valha a pena usar a posição absoluta dos videos na página, ao inves do css, pois aparentemente este muda.
+    videoAssistido = 2
+    if(videoAssistido == 1):
+        video = driver.find_element(By.CLASS_NAME, 'ScCoreLink-sc-16kq0mq-0 jRnnHH tw-link')
+    elif(videoAssistido == 2):
+        video = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/div/main/div[1]/div[3]/div/div/div/div/div[1]/div[1]/div[3]/div/div/div/div/div[1]/div/strong/a')
+    else:
+        video = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/div/main/div[1]/div[3]/div/div/div/div/div[1]/div[1]/div[4]/div/div/div/div/div[1]/div/strong/a')
+
+    video.click()
+    time.wait(tempoDeVisualizacao)
 
 try:
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chromeOptions)
@@ -127,18 +196,13 @@ except:
     print("Erro ao logar no Twitch ou Login já realizado")
 
 try:
-    first_channel = driver.find_element(By.XPATH, '(//div[@class="Layout-sc-1xcs6mc-0 cwtKyw side-nav-card"])[1]/a')
-    first_channel.click()
-    print("Primeiro canal clicado com sucesso!")
+    Treino()
 except Exception as e:
     print(f"Erro ao clicar no canal: {e}")
     try:
-        # Localizar o elemento pelo atributo `data-a-target`
-        videoPrincipal = driver.find_element(By.CSS_SELECTOR, 'div[data-a-target="player-overlay-click-handler"]')
-        
-        # Clicar no elemento
-        videoPrincipal.click()
-        print("Elemento clicado com sucesso!")
+        first_channel = driver.find_element(By.XPATH, '(//div[@class="Layout-sc-1xcs6mc-0 cwtKyw side-nav-card"])[1]/a')
+        first_channel.click()
+        print("Primeiro canal clicado com sucesso!")
     except Exception as e:
         print(f"Erro ao clicar no elemento: {e}")
         pass
@@ -147,3 +211,11 @@ except Exception as e:
 #Tempo de visualização
 time.sleep(random.uniform(540.0, 720.0))
 driver.quit()
+
+"""
+#TO DO
+- Registrar informações sobre o treino em um csv
+- Adicionar um sistema de log
+- Possibilitar o treino de mais de uma persona
+- tornar o programa um script que será rodado por x tempo e depois encerrado
+"""
