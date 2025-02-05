f5023686878a77b6d50e3f10628a9886965721cc
flaviosoriano <flaviosoriano0@gmail.com>
2025-02-05 09:48:02 -0300

    test CSV

--- Persona/utils/csv_operations.py
+++ Persona/utils/csv_operations.py
@@ -1,10 +1,10 @@
 import csv
 import datetime
 
-def registrar_dados(csv_file, jogo, duracao):
+def registrar_dados(csv_file, channelData, duracao):
     with open(csv_file, mode='a', newline='') as file:
         writer = csv.writer(file)
         # Escreve o cabeçalho apenas se o arquivo estiver vazio
         if file.tell() == 0:
             writer.writerow(["Data", "Jogo", "Duração (segundos)"])
-        writer.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), jogo, duracao])
+        writer.writerow([datetime.now().strftime("%Y-%m-%d"), channelData.channel_name, duracao])
