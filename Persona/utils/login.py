1158b4dbf414798de6a0c83c62e9bbd375e04be1
flaviosoriano <flaviosoriano0@gmail.com>
2025-03-17 16:02:56 -0300

    Add: connect to nordvpn

--- Persona/utils/login.py
+++ Persona/utils/login.py
@@ -2,15 +2,16 @@ from math import e
 import random
 import time
 
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
 
+def NordVpnLogin(numero_servidor):
+    script = "nordvpn connect us" + str(numero_servidor)
+    output = os.popen(script).read()
+    print(output)
 
 def ChromeLogin(driver, google_login, google_password):
     #fazer login na conta google
