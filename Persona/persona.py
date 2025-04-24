dd26d1569767041db9b69e34d9cb22c3d9bec47a
Flavio Soriano <flaviosoriano0@gmail.com>
2025-04-23 22:22:10 -0300

    tempo+

--- Persona/persona.py
+++ Persona/persona.py
@@ -60,13 +60,13 @@ if(br):
 faixaEtaria = 0 # ----Mudar para a faixa etária desejada----
 
 if faixaEtaria == 0: # 12-
-    tempo_min = 300
-    tempo_max = 600 
+    tempo_min = 600
+    tempo_max = 1200 
     jogosAssistir = allJogos
 
 if faixaEtaria == 1:
-    tempo_min = 300
-    tempo_max = 600
+    tempo_min = 600
+    tempo_max = 900
     jogosAssistir = allJogos
 
 # Configurar Logs
